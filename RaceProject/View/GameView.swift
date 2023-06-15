d6b7cda4e557ae2f9cc1b8f6bbb313a93f63e874
Arseni Khatsuk <khatsuk007@gmail.com>
2023-06-15 17:01:43 +0300

    -implemented animation for road

--- RaceProject/View/GameView.swift
+++ RaceProject/View/GameView.swift
@@ -9,21 +9,22 @@ import UIKit
 
 final class GameView: UIView {
     
-    private let musicManager = MusicManager()
     private let screenWidth = UIScreen.main.bounds.width
-    private let screenHeight = UIScreen.main.bounds.height
+    let screenHeight = UIScreen.main.bounds.height
     
     private var stripeWidth: CGFloat { screenWidth * 0.03 }
-    private var stripeHeight: CGFloat { screenHeight * 0.10 }
+    var stripeHeight: CGFloat { screenHeight * 0.10 }
     private var horizontalSpacing: CGFloat { screenWidth * 0.25 }
-    private var verticalSpacing: CGFloat { screenHeight * 0.05 }
+    var verticalSpacing: CGFloat { screenHeight * 0.05 }
     private var sidePadding: CGFloat { screenWidth * 0.05 }
     
-    private lazy var horizontalStackView: UIStackView = {
-        let horizontalStackView = UIStackView()
+     lazy var horizontalStackView: UIStackView = {
+        var horizontalStackView = UIStackView()
         horizontalStackView.axis = .horizontal
         horizontalStackView.distribution = .fillEqually
         horizontalStackView.spacing = horizontalSpacing
+        let stackViewWidth = screenWidth - 2 * sidePadding
+        horizontalStackView.frame = CGRect(x: sidePadding, y: 0, width: stackViewWidth, height: screenHeight)
         return horizontalStackView
     }()
     
@@ -61,16 +62,15 @@ final class GameView: UIView {
     private func configure() {
         setUpViews()
         setUpConstraints()
-        musicManager.play()
     }
     
     private func setUpViews() {
         backgroundColor = UIColor.lightGray
         
         let numberOfRows = Int((screenHeight - stripeHeight) / (stripeHeight + verticalSpacing))
-    
+        
         [horizontalStackView, carImageView ,leftArrowButton, rightArrowButton].forEach {addSubview($0)}
-        [horizontalStackView, carImageView ,leftArrowButton, rightArrowButton].forEach {$0.translatesAutoresizingMaskIntoConstraints = false}
+        [carImageView ,leftArrowButton, rightArrowButton].forEach {$0.translatesAutoresizingMaskIntoConstraints = false}
         
         for _ in 0..<4 {
             let verticalStackView = createVerticalStackView(numberOfRows: numberOfRows)
@@ -104,11 +104,6 @@ final class GameView: UIView {
     
     private func setUpConstraints() {
         NSLayoutConstraint.activate([
-            horizontalStackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: sidePadding),
-            horizontalStackView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -sidePadding),
-            horizontalStackView.topAnchor.constraint(equalTo: topAnchor),
-            horizontalStackView.bottomAnchor.constraint(equalTo: bottomAnchor),
-            
             carImageView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -143),
             carImageView.widthAnchor.constraint(equalToConstant: 50),
             carImageView.heightAnchor.constraint(equalToConstant: 103),
@@ -118,7 +113,7 @@ final class GameView: UIView {
             leftArrowButton.widthAnchor.constraint(equalToConstant: 90),
             leftArrowButton.heightAnchor.constraint(equalToConstant: 90),
             leftArrowButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 25),
-
+            
             rightArrowButton.bottomAnchor.constraint(equalTo: bottomAnchor,constant:  UIScreen.main.bounds.height * -0.035),
             rightArrowButton.widthAnchor.constraint(equalToConstant: 90),
             rightArrowButton.heightAnchor.constraint(equalToConstant: 90),
--- RaceProject/View/GameViewController.swift
+++ RaceProject/View/GameViewController.swift
@@ -13,8 +13,40 @@ class GameViewController: UIViewController {
         return GameView()
     }()
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    var musicManager: MusicManager = {
+        return MusicManager()
+    }()
+    
+    override func loadView() {
+        super.loadView()
         view = gameView
+       musicManager.play()
+    }
+    
+    override func loadViewIfNeeded() {
+        super.loadViewIfNeeded()
+        animateStripeDrop()
+    }
+    
+    func animateStripeDrop() {
+        let screenHeight = gameView.screenHeight * 2
+        let stripeHeight = gameView.stripeHeight
+        let verticalSpacing = gameView.verticalSpacing
+        let totalAnimationDuration = Double(screenHeight / stripeHeight) * 0.5
+
+        for verticalStackView in gameView.horizontalStackView.arrangedSubviews {
+            guard let stackView = verticalStackView as? UIStackView else { continue }
+
+            for (index, stripeView) in stackView.arrangedSubviews.enumerated() {
+                let initialPosition = CGPoint(x: stripeView.frame.origin.x, y: gameView.horizontalStackView.frame.origin.y + CGFloat(index) * (stripeHeight + verticalSpacing) - stripeHeight)
+                let finalPosition = CGPoint(x: stripeView.frame.origin.x, y: screenHeight + CGFloat(index) * (stripeHeight + verticalSpacing) - stripeHeight)
+
+                stripeView.frame.origin = initialPosition
+
+                UIView.animate(withDuration: totalAnimationDuration, delay: Double(index) * 0.1, options: [.curveLinear, .repeat], animations: {
+                    stripeView.frame.origin = finalPosition
+                }, completion: nil)
+            }
+        }
     }
 }
